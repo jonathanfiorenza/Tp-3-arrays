17ea1dc10bc61d3c4b8cbe77162e4444ab4c16cf
jonathan <jonathanfiorenza6@gmail.com>
2025-09-15 18:35:51 -0300

    agregamos un programa q nos diga el valor de los lados de un rectangulo, y luego su perimetro

---
+++ js/06-ejercicio.js
@@ -0,0 +1,16 @@
+/*
+Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, luego crear una función para calcular
+ su perímetro y mostrarlo por pantalla.
+
+La fórmula del perímetro  es p = 2*(a +b)
+
+
+*/
+
+const calcularPerimetro = (ladoA, ladoB) => { //calcularPerimetro es el nombre de la funcion SIRVE PARA CALCULAR EL PERIMETRO DE UN RECTANGULO
+    return 2 * (ladoA + ladoB); //formula del perimetro el * es para multiplicar
+}
+const ladoA = parseFloat(prompt("Ingrese el valor del lado A del rectángulo:")); //parseFloat convierte el valor ingresado en un numero con decimales
+const ladoB = parseFloat(prompt("Ingrese el valor del lado B del rectángulo:"));
+const perimetro = calcularPerimetro(ladoA, ladoB);
+document.writeln("<p>El perímetro del rectángulo es: " + perimetro + "</p>");   
